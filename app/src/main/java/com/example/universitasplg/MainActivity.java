2fae2189ae0380d7e4ca193a4c4720fe07137258
Adikaraalff <73709213+Adikaraalff@users.noreply.github.com>
2022-12-19 10:54:10 +0700

    update

--- app/src/main/java/com/example/universitasplg/MainActivity.java
+++ app/src/main/java/com/example/universitasplg/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.universitasplg;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvUniversitas;
+    private ArrayList<ModelUniversitas> data = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvUniversitas = findViewById(R.id.rv_universitas);
+        rvUniversitas.setHasFixedSize(true);
+
+        data.addAll(DataUniversitas.DataUniveristas());
+        tampilDataCard();
+        //tampilDataGrid();
+    }
+
+    private void tampilDataCard(){
+        rvUniversitas.setLayoutManager(new LinearLayoutManager((this)));
+        AdapterCard varAdapterCard = new AdapterCard(data, MainActivity.this);
+        rvUniversitas.setAdapter(varAdapterCard);
+    }
+
+    private void tampilDataGrid() {
+        rvUniversitas.setLayoutManager(new GridLayoutManager(this, 2));
+        AdapterGrid varAdapterGrid = new AdapterGrid(data, MainActivity.this);
+        rvUniversitas.setAdapter(varAdapterGrid);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_mode, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_mode_card:
+                tampilDataCard();
+                break;
+            case R.id.menu_mode_grid:
+                tampilDataGrid();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
