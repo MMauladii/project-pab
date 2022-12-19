037ec2ba950f9fd56a4f926f88732a67781c37c4
Adikaraalff <73709213+Adikaraalff@users.noreply.github.com>
2022-12-19 10:45:47 +0700

    Update DetailActivity.java

--- app/src/main/java/com/example/universitasplg/DetailActivity.java
+++ app/src/main/java/com/example/universitasplg/DetailActivity.java
@@ -1,4 +1,40 @@
 package com.example.universitasplg;
 
-public class DetailActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+public class DetailActivity extends AppCompatActivity {
+    private ImageView ivFoto;
+    private TextView tvNama, tvTentang;
+    private String yNama, yTentang, yFoto, yLogo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        ivFoto = findViewById(R.id.iv_foto);
+        tvNama = findViewById(R.id.tv_nama);
+        tvTentang = findViewById(R.id.tv_tentang);
+
+        Intent ambil = getIntent();
+        yNama = ambil.getStringExtra("xNama");
+        yTentang = ambil.getStringExtra("xTentang");
+        yFoto = ambil.getStringExtra("xFoto");
+        yLogo = ambil.getStringExtra("xLogo");
+
+        tvNama.setText(yNama);
+        tvTentang.setText(yTentang);
+
+        Glide
+                .with(DetailActivity.this)
+                .load(yFoto)
+                .into(ivFoto);
+    }
 }
